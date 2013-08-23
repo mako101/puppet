ef4760e3f474a4760d98070c4595b3dcf5f0bfce
git <git@beta.example.net>
2013-08-24 00:58:55 +0100

    sdfhsdhf

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -27,8 +27,31 @@ node 'beta.example.net' {
   user { 'art':
     ensure  => present,
     comment => 'Art Vanderlay',
-    home    => '/home/art'
+    home    => '/home/art',
     managehome => true,
-    password => password,
   }
+
+
+  ssh_authorized_key { 'art_ssh':
+    user    => 'art',
+    type    => 'rsa',
+    key     =>  'AAAAB3NzaC1yc2EAAAABIwAAAQEAsdNfFRoPxC5HJWgxc6rsJlPhDb1pDXymhI5ih5KFP8bfFDVfix2/PvRU9MjY5IetEZxIaXrzZ8395eo/pvjY6qXOLpmL9p9QmLaqWiio9Li7EeWS3jjGcTg5iAf7KgcEROEb9Zb6eNOCmtvyoKJAztn3fb4cNTirL/9LtUEWHQvadATZtfFa76eDaK78BJ0yZZDYUa7SH219ZunMMalkLZJneNIay4+s73X/EDnzoG1WPre4ah2vTNDzhfoYWp8qWcw5WncydyyvA3JwljZWjYrJUjcnXhOxAqtjKBFccT7W67Uym6zDzQP3HQm1KPZg/9LxzZQzOxxljHkmtMTTRQ==',
+  }
+
+  # Disabled ssh key
+  ssh_authorized_key { 'john_ssh':
+    user    => 'john',
+    type    => 'rsa',
+    key     => '',
+  }  
+
+  #Disabled user
+  user { 'john':
+    ensure  => present,
+    comment => 'John Simmons',
+    home    => '/home/john',
+    managehome => true,
+    password => '*',
+  }
+
 }
