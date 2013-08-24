b7eecf62804762be80d8fd575025ccd723a1342b
Viktor Mendes <vmj100@gmail.com>
2013-08-24 15:03:52 -0700

    adding trigger command + file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -61,8 +61,15 @@ node 'gamma.example.net' {
     key     =>  'AAAAB3NzaC1yc2EAAAABIwAAAQEAsdNfFRoPxC5HJWgxc6rsJlPhDb1pDXymhI5ih5KFP8bfFDVfix2/PvRU9MjY5IetEZxIaXrzZ8395eo/pvjY6qXOLpmL9p9QmLaqWiio9Li7EeWS3jjGcTg5iAf7KgcEROEb9Zb6eNOCmtvyoKJAztn3fb4cNTirL/9LtUEWHQvadATZtfFa76eDaK78BJ0yZZDYUa7SH219ZunMMalkLZJneNIay4+s73X/EDnzoG1WPre4ah2vTNDzhfoYWp8qWcw5WncydyyvA3JwljZWjYrJUjcnXhOxAqtjKBFccT7W67Uym6zDzQP3HQm1KPZg/9LxzZQzOxxljHkmtMTTRQ==',
   }
 
-  exec { 'Random Command':
+  file {'/tmp/test.conf':
+    ensure => present,
+    content => 'a',
+  }
+
+  exec { 'Config Update':
    command => '/bin/echo I ran this command on `/bin/date` > /tmp/command.txt',
+   refreshonly => true,
+   subscribe   => File['/tmp/test.conf'],
   }
  
   exec { 'Copy Once':
