fa36c8146fc45d2a4927f28874788489ca135889
Viktor Mendes <vmj100@gmail.com>
2013-09-07 14:29:14 +0100

    adding processor count to hardware

--- modules/hardware/manifests/init.pp
+++ modules/hardware/manifests/init.pp
@@ -1,12 +1,20 @@
 class hardware {
   
-  if $::memorysize_mb >= 4000 {
+  if $::memorysize_mb >= 8000 {
     notify { 'This machine has a lot of RAM': }
   }
-    elsif $::memorysize_mb >= 2000 {
+    elsif $::memorysize_mb >= 4000 {
     notify { 'This machine has moderate amount of RAM': }    
   }
-    else { notify { 'This machine has little RAM': }
+    else { 
+    notify { 'This machine has little RAM': }
   }
   
 }
+
+  if $::processorcount <= 2 {
+    notify { 'This is a single-core machine': }
+  } 
+    else { 
+    notify { 'This is a multi-core machine': } 
+  }
