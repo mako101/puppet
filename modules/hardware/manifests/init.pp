a74cf6a73c81755b87c95488a168d47b6dcb5ad7
Viktor Mendes <vmj100@gmail.com>
2013-09-09 22:46:05 +0100

    Adding case test

--- modules/hardware/manifests/init.pp
+++ modules/hardware/manifests/init.pp
@@ -1,4 +1,6 @@
 class hardware {
+ 
+# Testing the if statements 
   
   if $::memorysize_mb >= 8000 {
     notify { 'This machine has a lot of RAM': }
@@ -20,4 +22,21 @@ class hardware {
   unless $::kernel == 'Windows' {
     notify { 'This machine runs a Linux kernel': }  
   }
+  
+# Testing the case statements  
+  
+  case $::operatingsystem {
+    'CentOS', 'RedHat': {
+      notify { 'This is a Red Hat derivative': }
+    }
+    'Debian', 'Ubuntu': {
+      notify { 'This is a Debian derivative': }
+    }
+    default: { fail('Puppet cannot recognise the installed OS') 
+    }
+  }
+    
+  
+  
+  
 }
