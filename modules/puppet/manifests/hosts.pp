02fc4c682cdf6e0997110c6a6fcff841c7af3e21
Viktor Mendes <vmj100@gmail.com>
2013-11-11 22:12:25 +0000

    Added host aliases

--- modules/puppet/manifests/hosts.pp
+++ modules/puppet/manifests/hosts.pp
@@ -6,14 +6,17 @@ class puppet::hosts {
   }
   
   host {'ipa.example.net':
-    ip => '192.168.122.10',
-  }
+    ip           => '192.168.122.10',
+    host_aliases => 'ipa',
+    }
   
   host {'beta.example.net':
-    ip => '192.168.122.20',
+    ip           => '192.168.122.20',
+    host_aliases => 'beta', 
   }
 
   host {'gamma.example.net':
-    ip => '192.168.122.30',
+    ip           => '192.168.122.30',
+    host_aliases => 'gamma',
   }
 }
