c7edb89d3ec5367595ea167aaf59f4bc52a77fda
Viktor Mendes <viktor.mendes@o2.com>
2015-10-25 13:43:40 +0000

    started on puppet 4 features

---
+++ modules/p4/manifests/init.pp
@@ -0,0 +1,43 @@
+
+include p4
+# require stdlib
+
+class p4 {
+	
+	$a = {
+      home   => 'base',
+      number => 'one',
+      tick   => 'tock'
+	}
+   
+   $config = @(END) 
+     This is a test template
+     <%= @key %> = <%= @value %>
+   END
+
+
+   file {'/home/viktor/testdir':
+     ensure => directory,
+   }
+
+   $a.each |$key, $value| {
+
+    file { "/home/viktor/testdir/${key}":
+      ensure => present,
+      content => inline_template($config),
+      require => File['/home/viktor/testdir'],
+    }
+
+     # notify { "The key is ${key}, the value is ${value} \n\n": } 
+   }
+
+
+     # $b = ['a', 'b', 'foo']
+
+     # $b.each |$val| {
+     # 	notify { "The value is ${val} \n": }
+     # }
+
+
+
+}
